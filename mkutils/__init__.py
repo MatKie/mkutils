5d22a6d86ab6fd21a5177a08ff36379df4d0ec3d
MatKie <mtth.kiesel@gmail.com>
2020-05-19 21:05:54 +0100

    Typos in __init__.py

--- mkutils/__init__.py
+++ mkutils/__init__.py
@@ -3,5 +3,5 @@ from .plot_gromacs import PlotGromacs
 from .plot_sims import PlotSims
 from .chunk_data import ChunkData
 from .replica import EvalReplica
-from .plot_function import save_to_file
+from .plot_functions import save_to_file
 from .plot_functions import create_fig
